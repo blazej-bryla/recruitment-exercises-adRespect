d4a2c0315ca508fac3e71ba225d1589e2b4065ec
blazej-bryla <brykuski@gmail.com>
2023-08-16 09:48:52 +0200

    feat: create tailwindcss config for project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {}
+    extend: {
+      maxWidth: {
+        project: 1440
+      },
+      colors: {
+        green: '#1B5B31',
+        gray: '#F5F0EC',
+        beige: '#DCC1AB',
+        black: '#111111'
+      },
+      textColor: {
+        white: '#F5F0EC'
+      }
+    }
   },
   plugins: []
 }
