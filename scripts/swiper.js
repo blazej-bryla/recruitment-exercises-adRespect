80f64242fd8a3c974eacbb0fc823ae8c81807e38
blazej-bryla <brykuski@gmail.com>
2023-08-17 21:40:36 +0200

    fix: fix build error

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,12 +1,11 @@
 import Swiper from 'swiper'
-// import Swiper and modules styles
 import { Navigation, Pagination } from 'swiper/modules'
 
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
-new Swiper('.mySwiper', {
+const swiper = new Swiper('.mySwiper', {
   modules: [Navigation, Pagination],
   navigation: { nextEl: '.slider-next', prevEl: '.slider-prev' }
 })
